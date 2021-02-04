0bd8f67f8371387b23baa1858a3c1a80276da556
Flávio Martins da Silva <flaviomartins_silva@hotmail.com>
2021-02-04 14:38:42 -0300

    fix: router redirect

--- next.config.js
+++ next.config.js
@@ -1,2 +1,23 @@
 const withImages = require("next-images");
-module.exports = withImages();
+const withPlugins = require("next-compose-plugins");
+
+const nextConfig = {
+  target: "serverless",
+  webpack(config, { isServer, webpack }) {
+    return config;
+  },
+};
+
+const redirects = {
+  async redirects() {
+    return [
+      {
+        source: "/old/blogs/:slug*",
+        destination: "whatever your new rewrite url",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = withPlugins([[withImages], [redirects]], nextConfig);
